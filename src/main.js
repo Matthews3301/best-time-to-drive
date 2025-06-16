446f3984365fc4e07bfdb034fa51c7c3d0f5a8e7
Matthews3301 <salamon.mat@gmail.com>
2025-06-16 15:19:06 +0100

    vercel

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+import { Analytics } from '@vercel/analytics/vue'
 import App from './App.vue'
 
-createApp(App).mount('#app') 
+const app = createApp(App)
+app.use(Analytics)
+app.mount('#app') 
